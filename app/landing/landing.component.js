edf33402604f0624b55dc1c532d460d1c09922fa
MOLLY BROOKS <MOLLYJBROOKS@GMAIL.COM>
2017-04-19 20:23:23 -0600

    added thoughts

--- app/landing/landing.component.js
+++ app/landing/landing.component.js
@@ -8,3 +8,19 @@
         })
 
 })();
+
+function getInfo(){
+    var url = "https://bo.reddit.com/r/showerthoughts.json"
+    var title = []
+    $.get(url)
+      .then(function(data) {
+        title.push(data.data.children.data.title)
+				console.log(title)
+				updatePage(title)
+      })
+}
+			function updatePage(title){
+			  $(".showerthought-container").append("<h4>" + title + "</h4>")
+			    }
+
+				getInfo()
